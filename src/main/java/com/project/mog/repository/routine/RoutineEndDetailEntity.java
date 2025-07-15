da52862c78ea0f5fe57f7cb9e7e163e1246c8d13
김원진 <76833065+oen0thera@users.noreply.github.com>
2025-07-15 20:57:33 +0900

    feat : 루틴 상세 추가,삭제 api 구현, 루틴 결과 entity 구현
    1. 루틴 상세 추가,삭제 api 구현
    - 루틴 상세 추가 api
      - api/v1/routine/1/save로 POST 요청
      - body: {
        "exId",
        "srName",
        "setNumber",
        "reps",
        "weight"
    }
    - 루틴 상세 삭제 api
      - api/v1/routine/save/1로 DELETE 요청
    2. 루틴 결과 관련 entity 구현
    - 루틴 결과 관련 entity 구현
    3. 기타
    - 루틴 상세 일괄 등록 등 프론트 루틴 json 데이터에 따라 구현 예정

---
+++ src/main/java/com/project/mog/repository/routine/RoutineEndDetailEntity.java
@@ -0,0 +1,46 @@
+package com.project.mog.repository.routine;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="routineenddetail")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoutineEndDetailEntity {
+	@Id
+	@Column(length=19,nullable=false)
+	@SequenceGenerator(name = "SEQ_RED_GENERATOR",sequenceName = "SEQ_RED",allocationSize = 1,initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator ="SEQ_RED_GENERATOR" )	
+	private long redId;
+	
+	@Column(length=19)
+	private String srName;
+	
+	@Column(length=19)
+	private long setNumber;
+	
+	@Column(length=19)
+	private long reps;
+	
+	@Column(length=19)
+	private long weight;
+	
+}
